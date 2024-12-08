310e18f35bae5df85a72a20e263e588e2a640302
Untung <u.untung0029@gmail.com>
2024-12-08 16:53:20 +0800

    Header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import { createTheme, MantineProvider } from "@mantine/core";
 import "./App.css";
 import "@mantine/core/styles.css";
+import Header from "./components/Header/Header";
 
 const theme = createTheme({});
 
 function App() {
-  return <MantineProvider theme={theme}></MantineProvider>;
+  return (
+    <MantineProvider theme={theme}>
+      <Header></Header>
+    </MantineProvider>
+  );
 }
 
 export default App;
