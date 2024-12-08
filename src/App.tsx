3df9ff71cab38159cae9fd74b78e8dd618d403bb
Untung <u.untung0029@gmail.com>
2024-12-08 16:24:45 +0800

    Setup mantine

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
+import { createTheme, MantineProvider } from "@mantine/core";
 import "./App.css";
+import "@mantine/core/styles.css";
+
+const theme = createTheme({});
 
 function App() {
-  return <></>;
+  return <MantineProvider theme={theme}></MantineProvider>;
 }
 
 export default App;
